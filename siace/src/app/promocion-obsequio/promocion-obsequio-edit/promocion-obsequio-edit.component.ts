1f275f7865f035383d9f2e6df1ac5784278dcef4
luissd0ev <luissdev@outlook.com>
2025-09-25 11:14:18 -0600

    busqueda predictiva en promocion obsequio

--- siace/src/app/promocion-obsequio/promocion-obsequio-edit/promocion-obsequio-edit.component.ts
+++ siace/src/app/promocion-obsequio/promocion-obsequio-edit/promocion-obsequio-edit.component.ts
@@ -1,11 +1,13 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 import { PromocionObsequioService } from '../promocion-obsequio.service';
 import { PromocionObsequio } from '../promocion-obsequio';
-import { map, switchMap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { ProductoService } from '../../producto/producto.service';
 
 @Component({
    selector: 'app-promocion-obsequio-edit',
@@ -22,27 +24,107 @@ export class PromocionObsequioEditComponent implements OnInit {
    id!: string;
    promocionObsequio!: PromocionObsequio;
 
+   // Lista de productos para autocomplete
+   listProductos: any[] = [];
+   
+   // Control para autocomplete de productos
+   productoControl = new FormControl('');
+   
+   // Observable filtrado para productos
+   filteredProductos!: Observable<any[]>;
 
    /* Constructores */
    
    constructor(
       private dialogRef: MatDialogRef<PromocionObsequioEditComponent>,
       private promocionObsequioService: PromocionObsequioService,
-	   private toastr: ToastrService,
+      private toastr: ToastrService,
+      private productoService: ProductoService,
       @Inject(MAT_DIALOG_DATA) public data: any) {
-      this.promocionObsequio=data.promocionObsequio;
+      this.promocionObsequio = data.promocionObsequio;
    }
-
 
    ngOnInit() {
+      this.loadProductos();
    }
 
+   loadProductos() {
+      // Cargar productos
+      this.productoService
+         .find({
+            proId: '0',
+            proFamId: '0',
+            proPreId: '0',
+            proActivo: 'all',
+         })
+         .subscribe((res) => {
+            this.listProductos = res;
+            this.setupAutocomplete();
+            this.setInitialValues();
+         });
+   }
+
+   setupAutocomplete() {
+      if (this.listProductos.length > 0) {
+         // Autocomplete para productos
+         this.filteredProductos = this.productoControl.valueChanges.pipe(
+            startWith(''),
+            map((value) => {
+               const filterValue = this._getFilterValue(value);
+               return this._filterProductos(filterValue);
+            })
+         );
+      }
+   }
+
+   setInitialValues() {
+      // Establecer valor inicial basado en el ID existente
+      if (this.promocionObsequio.pobProId && this.listProductos.length > 0) {
+         const producto = this.listProductos.find(p => p.proId == this.promocionObsequio.pobProId);
+         if (producto) {
+            this.productoControl.setValue(producto);
+         }
+      }
+   }
+
+   private _getFilterValue(value: any): string {
+      if (typeof value === 'string') {
+         return value;
+      }
+      if (value && typeof value === 'object' && value.proNombre) {
+         return value.proNombre;
+      }
+      return '';
+   }
+
+   private _filterProductos(value: string): any[] {
+      const filterValue = value.toLowerCase();
+      return this.listProductos.filter((producto) => {
+         return (
+            (producto.proNombre &&
+               producto.proNombre.toLowerCase().includes(filterValue)) ||
+            (producto.proDescripcion &&
+               producto.proDescripcion.toLowerCase().includes(filterValue)) ||
+            producto.proId.toString().includes(filterValue)
+         );
+      });
+   }
+
+   displayProductoFn(producto: any): string {
+      return producto && producto.proNombre ? producto.proNombre : '';
+   }
+
+   onProductoSelected(producto: any) {
+      if (producto) {
+         this.promocionObsequio.pobProId = producto.proId;
+      }
+   }
 
    /*Métodos*/
    
    save() {
       this.promocionObsequioService.save(this.promocionObsequio).subscribe({
-         next:  result => {
+         next: result => {
             if (Number(result) > 0) {
                this.toastr.success('El promoción obsequio ha sido guardado exitosamente', 'Transacción exitosa');
                this.promocionObsequioService.setIsUpdated(true);
--- siace/src/app/promocion-obsequio/promocion-obsequio.module.ts
+++ siace/src/app/promocion-obsequio/promocion-obsequio.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
+import { MatNativeDateModule, MatOption } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
 
@@ -15,6 +15,8 @@ import { PromocionObsequioListComponent } from './promocion-obsequio-list/promoc
 import { PromocionObsequioEditComponent } from './promocion-obsequio-edit/promocion-obsequio-edit.component';
 import { PromocionObsequioService } from './promocion-obsequio.service';
 import { PromocionObsequioRoutingModule } from './promocion-obsequio.routing.module';
+import { MatAutocomplete, MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ProductoService } from '../producto/producto.service';
 
 @NgModule({
   imports: [
@@ -30,12 +32,16 @@ import { PromocionObsequioRoutingModule } from './promocion-obsequio.routing.mod
     MatDialogModule,
     MatCardModule,
     PromocionObsequioRoutingModule,
+    MatAutocomplete,
+    MatOption,
+    ReactiveFormsModule,
+    MatAutocompleteModule
   ],
   declarations: [
     PromocionObsequioListComponent,
     PromocionObsequioEditComponent
   ],
-  providers: [PromocionObsequioService],
+  providers: [PromocionObsequioService, ProductoService],
   exports: []
 })
 export class PromocionObsequioModule { }
